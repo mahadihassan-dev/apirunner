c313d180a0be1e24bec085bea65fadf988f2c414
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2023-09-19 19:22:54 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /* @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    serverless: {
-        // Increase the timeout to 30 seconds (or any suitable value)
-        timeout: 30,
-    },
 };
 
 module.exports = nextConfig;
